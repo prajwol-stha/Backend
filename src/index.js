8a1162020ba58028fbc88e83e68beea71858bd2e
Prajwol Shrestha <prajwol.stha777@gmail.com>
2024-06-12 12:31:26 +0545

    custom api response

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 // import mongoose from "mongoose";
 // import {DB_NAME} from './constants'
@@ -31,3 +32,11 @@ dotenv.config({
 })
 
 connectDB()
+    .then(()=>{
+        app.listen(process.env.PORT || 8000,()=>{
+            console.log(`Server started at port:${PORT}`)
+        })
+    })
+    .catch((err)=>{
+        console.log("MONGODB CONNECTION IS FAILED:",err)
+    })
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+//promise
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>
+        {
+            Promise.resolve().catch((err)=>next(err))
+        }
+}
+
+export {asyncHandler}
+
+//trycatch
+// const asyncHandler=(fn)=>async (req,res,next)=>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message:err.message,
+//         })
+//     }
+// }
